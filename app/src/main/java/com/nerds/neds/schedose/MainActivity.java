302291553fcf70858920e2e2232e6df08215f1e2
Jeffrey Zhang <jeffreyzhang123@gmail.com>
2018-08-31 18:06:25 -0700

    Ned is a chumbis

--- app/src/main/java/com/nerds/neds/schedose/MainActivity.java
+++ app/src/main/java/com/nerds/neds/schedose/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         EditText text= (EditText) findViewById(R.id.TextBox);
+        //Ned is a chumbis
     }
 
 
