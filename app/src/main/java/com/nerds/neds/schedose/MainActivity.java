76fb03673781cbb200b48be4cdfe638142c4c0ae
Jeffrey Zhang <jeffreyzhang123@gmail.com>
2018-08-31 18:11:16 -0700

    Makes the button work

--- app/src/main/java/com/nerds/neds/schedose/MainActivity.java
+++ app/src/main/java/com/nerds/neds/schedose/MainActivity.java
@@ -2,6 +2,7 @@ package com.nerds.neds.schedose;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText text= (EditText) findViewById(R.id.TextBox);
+        EditText text = (EditText) findViewById(R.id.TextBox);
+        Button pressbutton = (Button) findViewById(R.id.pressthistodie);
         //Ned is a chumbis
     }
 
