28108faba1250b45c001b15b31d9e5dd0d4c9f35
Ned Machulsky <ned.machulsky@gmail.com>
2018-08-31 18:31:30 -0700

    Added print text, but not working

--- app/src/main/java/com/nerds/neds/schedose/MainActivity.java
+++ app/src/main/java/com/nerds/neds/schedose/MainActivity.java
@@ -2,8 +2,10 @@ package com.nerds.neds.schedose;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText text = (EditText) findViewById(R.id.TextBox);
+
         Button pressbutton = (Button) findViewById(R.id.pressthistodie);
+
         //Ned is a chumbis
+        pressbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text = (EditText) findViewById(R.id.TextBox);
+                TextView result=(TextView) findViewById(R.id.textView2);
+                String number=pressbutton.getText().toString();
+                result.setText(number+"");
+            }
+        });
     }
 
 
