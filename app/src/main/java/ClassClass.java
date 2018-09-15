9519c216b3025d5a1ebbfac517e6012b69e16c97
Ned Machulsky <ned.machulsky@gmail.com>
2018-09-14 19:42:08 -0700

    Added ClassClass

---
+++ app/src/main/java/ClassClass.java
@@ -0,0 +1,47 @@
+public class ClassClass {
+    private String className;
+    private double length;
+    private int days,priority;
+    public ClassClass(String name,double Length,int day, int prior){
+        className = name;
+        length = Length;
+        days = day;
+        priority = prior;
+    }
+
+    public String getName(){
+        return className;
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public int getDay(){
+        return days;
+    }
+
+    public int getPriority(){
+        return priority;
+    }
+
+    public void setName(String name){
+        className = name;
+    }
+
+    public void setLength(double Length){
+        length = Length;
+    }
+
+    public void setDay(int day){
+        days = day;
+    }
+
+    public void setPriority(int prior){
+        priority = prior;
+    }
+    
+    public String toString(){
+        return "Name: " + className + " Length: " + length + "";
+    }
+}
---
+++ app/src/main/java/com/nerds/neds/schedose/CreateActivity.java
@@ -0,0 +1,13 @@
+package com.nerds.neds.schedose;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CreateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
