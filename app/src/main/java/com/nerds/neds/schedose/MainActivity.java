5a127b72000f9c616b2967583591c85f888e74a1
Ned Machulsky <ned.machulsky@gmail.com>
2018-10-03 16:41:04 -0700

    added storing name

--- app/src/main/java/com/nerds/neds/schedose/MainActivity.java
+++ app/src/main/java/com/nerds/neds/schedose/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nerds.neds.schedose;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,15 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ArrayList<ClassClass> x=new ArrayList<ClassClass>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String test="test";
+        String name = getSharedPreferences("username", Context.MODE_PRIVATE).getString("name", "user");
         TextView greeting=(TextView) findViewById(R.id.GreetingText) ;
-        greeting.setText("hi "+test);
+        greeting.setText("Hello "+name);
         Button nextpage = findViewById(R.id.nxtpage);
         nextpage.setOnClickListener(new View.OnClickListener() {
             @Override
