a7dfc88d5bab4a3161f96a8c74a5b2ae46d3097e
Jeffrey Zhang <jeffreyzhang123@gmail.com>
2018-10-03 18:23:17 -0700

    Puts ClassClass arraylist in sharedprefs

--- app/src/main/java/com/nerds/neds/schedose/MainActivity2.java
+++ app/src/main/java/com/nerds/neds/schedose/MainActivity2.java
@@ -1,5 +1,6 @@
 package com.nerds.neds.schedose;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,21 +9,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    ArrayList<ClassClass> classes = new ArrayList<ClassClass>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         EditText ClassName = findViewById(R.id.ClassName);
-        String classname=ClassName.getText().toString();
+        final String classname=ClassName.getText().toString();
         EditText ClassLength = findViewById(R.id.ClassLength);
-        Double Classlength=Double.parseDouble(ClassLength.getText().toString());
+        final Double Classlength=Double.parseDouble(ClassLength.getText().toString());
         Button Done = findViewById(R.id.Done);
         Done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                classes.add(new ClassClass(classname, Classlength, 0, 0));
+                Gson gson = new Gson();
+                String json = gson.toJson(classes);
+                getSharedPreferences("username", Context.MODE_PRIVATE).edit().putString("class", json).apply();
                 Intent StartIntent=new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(StartIntent);
             }
